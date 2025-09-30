1f5f129c13ae21c4c552fd8671bf2bc987ad4cd7
Anthony Canzona <anthonyscanzona@gmail.com>
2025-09-30 00:21:56 -0500

    cleanin up

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
+    #path('/', views.index, name='index'),
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
 ]
